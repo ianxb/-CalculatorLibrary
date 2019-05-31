0adb793e5a0ddff32cb4884f5debe78ab0aca8eb
bo xiao <bo.xiao@emc.com>
2019-05-31 15:09:56 +0800

    add delete

---
+++ delete.py
@@ -0,0 +1,7 @@
+"""
+add delete function
+"""
+
+
+def dell(first_item, second_item):
+    return first_item - second_item
---
+++ test_delete.py
@@ -0,0 +1,6 @@
+import delete
+
+
+class TestDelete:
+    def testde(self):
+        assert 8 == delete.dell(16,8)
